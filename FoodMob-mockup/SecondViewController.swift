133434f561a6c480b416a70d4ea0e71b04126b07
Jonathan Jemson <jonathanjemson@gatech.edu>
2015-11-23 13:15:18 -0500

    Initial Commit

---
+++ FoodMob-mockup/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FoodMob-mockup
+//
+//  Created by Jonathan Jemson on 11/23/15.
+//  Copyright © 2015 Jonathan Jemson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
