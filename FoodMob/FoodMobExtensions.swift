113b04c3afb27a1fcece11167582aa4523124bcc
Jonathan Jemson <jonathanjemson@gatech.edu>
2016-02-12 13:49:06 -0500

    Change a rawValue initializer to be more Swift-y.

--- FoodMob/FoodMobExtensions.swift
+++ FoodMob/FoodMobExtensions.swift
@@ -65,7 +65,7 @@ extension String {
     var isEmail: Bool {
         do {
             let regex = try NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: .CaseInsensitive)
-            return regex.firstMatchInString(self, options: NSMatchingOptions(rawValue: 0), range: NSMakeRange(0, self.characters.count)) != nil
+            return regex.firstMatchInString(self, options: [], range: NSMakeRange(0, self.characters.count)) != nil
         } catch {
             return false
         }
