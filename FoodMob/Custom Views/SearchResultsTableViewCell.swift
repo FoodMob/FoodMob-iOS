4116af03ecc3f09bb46ae80d0002fe214268e13d
James <94jamespark@gmail.com>
2016-03-20 01:44:36 -0400

    [UI] Modified the look of Search Results Scene [UI] Added image assets [UI] Cleaned up Storyboard [UI] Added custom TableViewCell

---
+++ FoodMob/Custom Views/SearchResultsTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResultsTableViewCell.swift
+//  FoodMob
+//
+//  Created by Jong Han Park on 3/17/16.
+//  Copyright © 2016 FoodMob. All rights reserved.
+//
+
+import UIKit
+class SearchResultsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var categories: UILabel!
+}
